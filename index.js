0dd32dd2098856d2a466863a72787a1523440265
erraX <niminjiecide@outlook.com>
2018-07-13 17:43:17 +0800

    update

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
+const path = require('path');
 const express = require('express')
+const proxy = require('http-proxy-middleware');
 const app = express();
 const mockMiddleware = require('./src/mockMiddleware');
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(mockMiddleware({
     whitelist: [/^\/bi/],
-    blacklist: [/\/bi\/create/],
-    remoteServer: {
-        hostname: '127.0.0.1',
-        port: 8888
-    }
+    blacklist: [],
+    mockFilePath: path.join(__dirname)
+}));
+
+app.use(proxy({
+    target: 'http://bi-mj.addev.com',
+    changeOrigin: true
 }));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
